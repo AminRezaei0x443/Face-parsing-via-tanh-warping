fb8a9876c1f7ee7caa057b9e12a9f55fa742041e
Amin Rezaei <66468076+AminRezaei0x443@users.noreply.github.com>
2021-02-10 02:30:27 +0330

    fix stage 2 lightning

--- model_lightning.py
+++ model_lightning.py
@@ -2,7 +2,7 @@ from resfpn import resnet_fpn_backbone
 from collections import OrderedDict
 from torchvision.ops.roi_align import RoIAlign
 from torch.nn import SmoothL1Loss, CrossEntropyLoss
-from dataset import new_HelenDataset
+from dataset import new_HelenDataset,Warped_HelenDataset
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
@@ -106,12 +106,16 @@ class Stage1(pl.LightningModule):
 
 class Hybird(pl.LightningModule):
     def __init__(self, hparam):
-        self.load_from_checkpoint(hparam['pretrain_path'])
+        super(Hybird, self).__init__()
+        self.args = hparam['args']
+        self.hparam = hparam
+        if hparam['pretrain_path'] != '':
+            self.load_from_checkpoint(hparam['pretrain_path'])
         self.backbone = BackBone()
         self.bbox_regress = ComponentRegress()
         self.roi_align = RoIAlign(output_size=(32, 32), spatial_scale=128. / 512., sampling_ratio=-1)
         self.inner_Module = inner_Module(num=4)
-        self.outer_Seg = outer_Seg(n_class=2)
+        self.outer_Seg = outer_Seg(n_class=3)
         self.criterion = CrossEntropyLoss()
 
     def forward(self, x):
@@ -120,10 +124,21 @@ class Hybird(pl.LightningModule):
 
         C, P = self.backbone(x)
         bbox = self.bbox_regress(C['4'])
+        """"" Mapping """
+        bbox = torch.tanh(bbox) * 256. + 256.
+        """" Bbox Padding """
+        # non-mouth
+        bbox[:3] += bbox[:3] * 0.05
+        # mouth
+        bbox[3] += bbox[3] * 0.1
+        """" Roi Align  """
+        # boxes Shape(10, 4, 4)
+        boxes = [bbox[:, i]
+                 for i in range(4)]
         # boxes(Tensor[K, 5] or List[Tensor[L, 4]])
-        rois = self.roi_align(input=P['2'], rois=bbox)
+        rois = self.roi_align(input=P['2'], rois=boxes)
         # roi (N, 4, 32, 32)
-        inner_pred = self.inner_Seg(rois)
+        inner_pred = self.inner_Module(rois)
         # inner_out is a OrderedDict
         # leye, reye, nose, mouth
         outer_pred = self.outer_Seg(P['2'])
@@ -131,9 +146,18 @@ class Hybird(pl.LightningModule):
         return inner_pred, outer_pred
 
     def training_step(self, batch, batch_idx):
-        image, labels = batch['image'], batch['labels']
-        pred = self(image)
-        loss = self.criterion(pred, labels.long())
+        # image, labels = batch['image'], batch['labels']
+        image, labels = batch['image'].to(self.device), batch['parts_labels']
+        outer_label, inner_label = labels['outer'], labels['inner']
+        in_pred, out_pred = self(image)
+        outer_label = outer_label.squeeze(1).to(self.device)
+        loss_out = self.criterion(out_pred, outer_label.long())
+        loss_in = []
+        for i, x in enumerate(['leye', 'reye', 'nose', 'mouth']):
+            loss_in.append(self.criterion(in_pred[x], inner_label[:, i].to(self.device).long()))
+        loss_in = torch.mean(torch.stack(loss_in))
+        # loss = self.criterion(pred, labels.long())
+        loss = loss_in + loss_out
         tqdm_dict = {'train_loss': loss}
         output = OrderedDict({
             'loss': loss,
@@ -143,33 +167,63 @@ class Hybird(pl.LightningModule):
         return output
 
     def validation_step(self, batch, batch_idx):
-        image, labels = batch['image'], batch['labels']
-        pred = self(image)
-        loss = self.criterion(pred, labels.long())
-        hist = self._fast_histogram(pred.argmax(dim=1, keepdim=False).cpu().numpy(),
-                                    labels.long().cpu().numpy(),
-                                    2, 2
-                                    )
+        # image, labels = batch['image'], batch['labels']
+        
+        image, labels = batch['image'].to(self.device), batch['parts_labels']
+        outer_label, inner_label = labels['outer'], labels['inner']
+        in_pred, out_pred = self(image)
+        outer_label = outer_label.squeeze(1).to(self.device)
+        loss_out = self.criterion(out_pred, outer_label.long())
+        loss_in = []
+        for i, x in enumerate(['leye', 'reye', 'nose', 'mouth']):
+            loss_in.append(self.criterion(in_pred[x], inner_label[:, i].to(self.device).long()))
+        loss_in = torch.mean(torch.stack(loss_in))
+        loss = loss_in + loss_out
+
+        # pred = self(image)
+        # loss = self.criterion(pred, labels.long())
+        # hist = self._fast_histogram(pred.argmax(dim=1, keepdim=False).cpu().numpy(),
+        #                             labels.long().cpu().numpy(),
+        #                             2, 2
+        #                             )
         output = OrderedDict({
             'val_loss': loss,
-            'val_hist': hist
+            # 'val_hist': hist
         })
         return output
+
+    def configure_optimizers(self):
+        if self.args.optim == 0:
+            self.optimizer = optim.Adam(self.parameters(), self.args.lr)
+        elif self.args.optim == 1:
+            self.optimizer = optim.SGD(self.parameters(), self.args.lr,
+                                       momentum=self.args.momentum, dampening=self.args.dampening,
+                                       weight_decay=self.args.decay, nesterov=False)
+        elif self.args.optim == 2:
+            self.optimizer = optim.SGD(self.parameters(), self.args.lr,
+                                       momentum=self.args.momentu, dampening=self.args.dampening,
+                                       weight_decay=self.args.decay, nesterov=True)
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.1)
+        return [self.optimizer], [self.scheduler]
 
     def validation_epoch_end(self, outputs):
         loss_list = []
         hist_list = []
         for out in outputs:
             loss_list.append(out['val_loss'])
-            hist_list.append(out['val_hist'])
-        mean_error = np.mean(loss_list)
-        hist_sum = np.sum(np.stack(hist_list, axis=0), axis=0)
-        A = hist_sum[1, :].sum()
-        B = hist_sum[:, 1].sum()
-        inter_select = hist_sum[1, 1]
-        F1 = 2 * inter_select / (A + B)
-        tqdm_dict = {'val_loss': mean_error, 'val_F1': F1}
-        result = {'progress_bar': tqdm_dict, 'log': tqdm_dict, 'avg_val_loss': tqdm_dict["val_loss"], 'val_F1': F1}
+            # hist_list.append(out['val_hist'])
+        mean_error = torch.mean(torch.tensor(loss_list))
+        # hist_sum = np.sum(np.stack(hist_list, axis=0), axis=0)
+        # A = hist_sum[1, :].sum()
+        # B = hist_sum[:, 1].sum()
+        # inter_select = hist_sum[1, 1]
+        # F1 = 2 * inter_select / (A + B)
+        tqdm_dict = {'val_loss': mean_error
+        # , 'val_F1': F1
+        }
+        result = {'progress_bar': tqdm_dict, 'log': tqdm_dict, 'avg_val_loss': tqdm_dict["val_loss"], 
+        # 'val_F1': F1
+        }
         return result
 
     def test_step(self, batch, batch_idx):
@@ -204,18 +258,29 @@ class Hybird(pl.LightningModule):
         return result
 
     def prepare_data(self):
-        self.train_data = new_HelenDataset(self.root_dir, 'train')
-        self.val_data = new_HelenDataset(self.root_dir, 'val')
-        self.test_data = new_HelenDataset(self.root_dir, 'test')
+        self.train_data = Warped_HelenDataset(root_dir=self.hparam['root_dir']['train'],
+                                           mode='train'
+                                           ,transform=self.hparam['transforms']['train']
+                                           )
+
+        self.val_data = Warped_HelenDataset(root_dir=self.hparam['root_dir']['val'],
+                                         mode='val'
+                                         , transform=self.hparam['transforms']['val']
+                                         )
+
+        self.test_data = Warped_HelenDataset(root_dir=self.hparam['root_dir']['test'],
+                                          mode='test'
+                                          ,transform=self.hparam['transforms']['test']
+                                          )
 
     def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=self.args.size, shuffle=True, num_workers=self.args.workers)
+        return DataLoader(self.train_data, batch_size=self.args.batch_size, shuffle=True, num_workers=self.args.workers)
 
     def val_dataloader(self):
-        return DataLoader(self.val_data, batch_size=self.args.size, shuffle=True, num_workers=self.args.workers)
+        return DataLoader(self.val_data, batch_size=self.args.batch_size, shuffle=True, num_workers=self.args.workers)
 
     def test_dataloader(self):
-        return DataLoader(self.test_data, batch_size=self.args.size, shuffle=True, num_workers=self.args.workers)
+        return DataLoader(self.test_data, batch_size=self.args.batch_size, shuffle=True, num_workers=self.args.workers)
 
     @staticmethod
     def _fast_histogram(a, b, na, nb):
@@ -251,25 +316,44 @@ class outer_Seg(pl.LightningModule):
         return x
 
 
+class inner_Module(pl.LightningModule):
+    def __init__(self, num):
+        super(inner_Module, self).__init__()
+        class_list = [3, 3, 2, 4]
+        self.segnets = nn.ModuleList([inner_Seg(class_list[r]) for r in range(num)])
+        self.names = ['leye', 'reye', 'nose', 'mouth']
+        self.num = num
+        self.outputs = OrderedDict()
+
+    def forward(self, x):
+        # Input x Shape(4 * N, 256, 32, 32)
+        x = x.view(self.num, -1, 256, 32, 32)
+        # Shape(4, N, 256, 32, 32)
+        for i in range(self.num):
+            self.outputs[self.names[i]] = self.segnets[i](x[i])
+        return self.outputs
+
+
 # Ouput Shape(N, n_class, 128, 128)
 class inner_Seg(pl.LightningModule):
     def __init__(self, n_class):
         super(inner_Seg, self).__init__()
         self.relu = nn.ReLU(inplace=True)
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv1 = nn.Conv2d(3, 256, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(256)
         self.conv2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(256)
         self.classifier = nn.Conv2d(256, n_class, kernel_size=1)
 
     def forward(self, x):
-        # x Shape(4 * N, 3, 32, 32)
+        # x Shape(N, 3, 32, 32)
         x = self.upsample(self.bn1(self.relu(self.conv1(x))))
         x = self.upsample(self.bn2(self.relu(self.conv2(x))))
         x = self.classifier(x)
+        # x Shape(N, C, 128, 128)
         return x
-
+        
 
 class ComponentRegress(pl.LightningModule):
     def __init__(self):
